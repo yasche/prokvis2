315e0dd0b167b81289c545582c795526af377677
yasche <172521093+yasche@users.noreply.github.com>
2025-12-27 13:54:37 +0100

    Add documentation for species_select_choices and its helper function

--- R/fct_species_selection_choices.R
+++ R/fct_species_selection_choices.R
@@ -2,16 +2,13 @@
 #'
 #' @description Create the species selection choices from the kinome_data object
 #'
-#' @return The return value, if any, from executing the function.
+#' @param kinome_data A kinome_data list object.
+#'
+#' @return A named character vector that contains the species to be used in a selectInput.
 #'
 #' @noRd
 species_selection_choices <- function(kinome_data) {
   spec_list <- unlist(purrr::map(kinome_data, extract_names))
-
-  #list(
-  #  unname(spec_names),
-  #  names(spec_names)
-  #)
 
   spec_names <- names(spec_list)
   names(spec_names) <- unname(spec_list)
@@ -20,6 +17,13 @@ species_selection_choices <- function(kinome_data) {
 }
 
 
+#' Helper function for joining species name and scientific name
+#'
+#' @param kinome A kinome from the kinome_data object.
+#'
+#' @returns A string in the following form: `scientific_name (name)` or `scientific_name` in case no `name` is available.
+#'
+#' @noRd
 extract_names <- function(kinome) {
   spec_name <- kinome$name
   spec_sc_name <- kinome$scientific_name
