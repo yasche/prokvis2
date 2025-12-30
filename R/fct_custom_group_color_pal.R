747c574630e80a6f039cba9c8497acb623970e5e
yasche <172521093+yasche@users.noreply.github.com>
2025-12-30 16:17:02 +0100

    Add documentation

--- R/fct_custom_group_color_pal.R
+++ R/fct_custom_group_color_pal.R
@@ -1,8 +1,13 @@
 #' custom_group_color_input
 #'
-#' @description A fct function
+#' @description Wrapper function to create the colourInputs for the individual groups if the palette is set to "Custom".
 #'
-#' @return The return value, if any, from executing the function.
+#' @param custom_color_nums A character vector containing the groups encoded by a unique number and the prefix "custom_group_col". Created with `kinase_groups_to_custom_color_numbers()`.
+#' @param kinase_groups A character vector containing the kinase group names. Created with `extract_kinase_groups()`.
+#' @param ns The namespace `ns`.
+#' @param id The module id.
+#'
+#' @return An input to be used in a UI.
 #'
 #' @noRd
 custom_group_color_input <- function(custom_color_nums, kinase_groups, ns, id) {
@@ -15,12 +20,30 @@ custom_group_color_input <- function(custom_color_nums, kinase_groups, ns, id) {
   )
 }
 
+#' extract_kinase_groups
+#'
+#' @description Helper function to extract the kinase groups
+#'
+#' @param kinome_df A `kinome_df` created with `extract_kinome_df()`
+#'
+#' @return A character vector containing the kinase groups.
+#'
+#' @noRd
 extract_kinase_groups <- function(kinome_df) {
   kinome_df %>%
     pull("Kinase_Group") %>%
     unique()
 }
 
+#' kinase_groups_to_custom_color_numbers
+#'
+#' @description Helper function to turn kinase groups into unique numbers with prefix "custom_group_col".
+#'
+#' @param kinase_groups A character vector containing the kinase groups, created with `extract_kinase_groups()`
+#'
+#' @return A character vector containing the kinase groups encoded by a unique number and prefix "custom_group_col".
+#'
+#' @noRd
 kinase_groups_to_custom_color_numbers <- function(kinase_groups) {
   paste0("custom_group_col", seq_len(length(kinase_groups)))
 }
