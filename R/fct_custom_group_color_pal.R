c9830ab2e70b4dd2dd13576fa675d1efbc961717
yasche <172521093+yasche@users.noreply.github.com>
2025-12-30 10:59:34 +0100

    Add helper functions for custom color palette

---
+++ R/fct_custom_group_color_pal.R
@@ -0,0 +1,26 @@
+#' custom_group_color_input
+#'
+#' @description A fct function
+#'
+#' @return The return value, if any, from executing the function.
+#'
+#' @noRd
+custom_group_color_input <- function(custom_color_nums, kinase_groups, ns) {
+  list(
+    #partly adapted from https://mastering-shiny.org/action-dynamic.html
+    "Choose a custom color for each group",
+    shiny::HTML("<br><br>"),
+    purrr::map(custom_color_nums, ~ colourpicker::colourInput(ns(.x), kinase_groups[as.numeric(stringr::str_remove(.x, "custom_group_col"))], value = "#BEBEBE"))
+  )
+}
+
+extract_kinase_groups <- function(kinome_df) {
+  kinome_df %>%
+    pull("Kinase_Group") %>%
+    unique()
+}
+
+kinase_groups_to_custom_color_numbers <- function(kinase_groups) {
+  paste0("custom_group_col", seq_len(length(kinase_groups)))
+}
+
