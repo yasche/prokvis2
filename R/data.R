bc7fe37c3124569a76fe054451b5b9f22feeff22
yasche <172521093+yasche@users.noreply.github.com>
2025-12-30 16:44:20 +0100

    Document `kinome_data`

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' A list object that contains kinome data for several model organisms
+#'
+#' @format ## `kinome_data`
+#' A list where the first level represents one species each.
+#'
+#' @source Contains data from several publications by Manning et al. who kindly allowed the redistribution of the data.
+"kinome_data"
