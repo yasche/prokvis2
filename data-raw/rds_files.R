1c5130378aff9062708ae567d61f8232d937407a
yasche <172521093+yasche@users.noreply.github.com>
2026-01-17 20:20:15 +0100

    Change internal `kinome_data`

--- data-raw/rds_files.R
+++ data-raw/rds_files.R
@@ -22,4 +22,19 @@ species_abr <- purrr::map(kinome_data,
 
 names(kinome_data) <- unlist(species_abr)
 
+# problems when plotting tetrahymena -> remove for now
+
+kinome_data[["tt"]] <- NULL
+
+# change order so that "important" species are on the top
+
+important_spec <- which(names(kinome_data) %in% c("dm", "mm", "hs", "sc"))
+
+kinome_data_order <- c(names(kinome_data)[important_spec], names(kinome_data)[-important_spec])
+
+setdiff(kinome_data_order, names(kinome_data))
+setdiff(names(kinome_data), kinome_data_order)
+
+kinome_data <- kinome_data[kinome_data_order]
+
 usethis::use_data(kinome_data, overwrite = TRUE)
